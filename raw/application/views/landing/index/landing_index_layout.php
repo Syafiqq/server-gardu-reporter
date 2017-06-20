d69d64cecad84a46f83222b2971b338d8eaf5cff
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-20 15:25:25 +0700

    add landing-index-layout

---
+++ raw/application/views/landing/index/landing_index_layout.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 20 June 2017, 3:12 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */ ?>
+
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="/assets/vendor/html5-boilerplate/dist/css/normalize.min.css">
+    <link rel="stylesheet" href="/assets/vendor/html5-boilerplate/dist/css/main.min.css">
+    <script src="/assets/vendor/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+<!-- Add your site or application content here -->
+<p>Hello world! This is HTML5 Boilerplate.</p>
+
+<script src="/assets/vendor/jquery/dist/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/assets/vendor/jquery/dist/jquery.min.js"><\/script>')</script>
+<script src="/assets/vendor/html5-boilerplate/dist/js/plugins.min.js"></script>
+
+</body>
+</html>
