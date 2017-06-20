4c391206e2dca12c13f86c6c95209b9efd2e5324
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-20 14:46:41 +0700

    Add some gulp task

--- gulpfile.dev.js
+++ gulpfile.dev.js
@@ -21,6 +21,49 @@ gulp.task('move-application-assets', function ()
         .pipe(gulp.dest('./application/'));
 });
 
+gulp.task('move-assets', function ()
+{
+    return gulp.src('./resources/assets/raw/**', {base: './resources/assets/raw/'})
+        .pipe(gulp.dest('./public/assets/'));
+});
+
+gulp.task('minify-img', function ()
+{
+    return gulp.src('./resources/assets/raw/**/{*.png,*.jpg,*.jpeg}', {base: './resources/assets/raw/'})
+        .pipe(gulp.dest('./public/assets/'));
+});
+
+gulp.task('minify-js', function (cb)
+{
+    pump([
+            gulp.src(['./resources/assets/raw/**/*.js', '!./resources/assets/raw/**/*.min.js'], {base: './resources/assets/raw/'})
+                .pipe(rename({
+                    suffix: ".min",
+                    extname: ".js"
+                })),
+            gulp.dest('./public/assets/')
+        ],
+        cb
+    );
+});
+
+gulp.task('minify-css', function ()
+{
+    return gulp.src(['./resources/assets/raw/**/*.css', '!./resources/assets/raw/**/*.min.css'], {base: './resources/assets/raw/'})
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(gulp.dest('./public/assets/'));
+});
+
+gulp.task('minify-html', function ()
+{
+    return gulp.src('./resources/assets/views/**/{*.php,*.html}', {base: './resources/assets/views/'})
+        .pipe(gulp.dest('./resources/views/'));
+});
+
+
 gulp.task('watch-move-application-assets', function ()
 {
 // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
@@ -28,5 +71,67 @@ gulp.task('watch-move-application-assets', function ()
     {
         return gulp.src('./raw/application/**', {base: './raw/application/'})
             .pipe(gulp.dest('./application/'));
+    });
+});
+
+gulp.task('watch-move-assets', function ()
+{
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('./resources/assets/raw/**', function ()
+    {
+        return gulp.src('./resources/assets/raw/**', {base: './resources/assets/raw/'})
+            .pipe(gulp.dest('./public/assets/'));
+    });
+});
+
+gulp.task('watch-minify-img', function ()
+{
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('./resources/assets/raw/**/{*.png,*.jpg,*.jpeg}', function ()
+    {
+        return gulp.src('./resources/assets/raw/**/{*.png,*.jpg,*.jpeg}', {base: './resources/assets/raw/'})
+            .pipe(gulp.dest('./public/assets/'));
+    });
+});
+
+gulp.task('watch-minify-js', function ()
+{
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch(['./resources/assets/raw/**/*.js', '!./resources/assets/raw/**/*.min.js'], function (cb)
+    {
+        pump([
+                gulp.src(['./resources/assets/raw/**/*.js', '!./resources/assets/raw/**/*.min.js'], {base: './resources/assets/raw/'})
+                    .pipe(rename({
+                        suffix: ".min",
+                        extname: ".js"
+                    })),
+                gulp.dest('./public/assets/')
+            ],
+            cb
+        );
+    });
+});
+
+gulp.task('watch-minify-css', function ()
+{
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch(['./resources/assets/raw/**/*.css', '!./resources/assets/raw/**/*.min.css'], function ()
+    {
+        return gulp.src(['./resources/assets/raw/**/*.css', '!./resources/assets/raw/**/*.min.css'], {base: './resources/assets/raw/'})
+            .pipe(rename({
+                suffix: ".min",
+                extname: ".css"
+            }))
+            .pipe(gulp.dest('./public/assets/'));
+    });
+});
+
+gulp.task('watch-minify-html', function ()
+{
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('./resources/assets/views/**/{*.php,*.html}', function ()
+    {
+        return gulp.src('./resources/assets/views/**/{*.php,*.html}', {base: './resources/assets/views/'})
+            .pipe(gulp.dest('./resources/views/'));
     });
 });
