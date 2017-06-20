9101841268e20125ae64a0de6708370b44abdedc
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-20 17:08:00 +0700

    Update layout adapting from admin-lte top-nav Remove unnecessary content for initial commit

--- raw/application/views/landing/index/landing_index_layout.php
+++ raw/application/views/landing/index/landing_index_layout.php
@@ -7,29 +7,108 @@
  * Github       : syafiqq
  */ ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html class="no-js" lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="This is landing page">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Landing Page</title>
-    <meta name="description" content="Gardu Reporter">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <!-- Place favicon.ico in the root directory -->
 
     <link rel="stylesheet" href="/assets/vendor/html5-boilerplate/dist/css/normalize.min.css">
     <link rel="stylesheet" href="/assets/vendor/html5-boilerplate/dist/css/main.min.css">
-    <script src="/assets/vendor/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js"></script>
-</head>
-<body>
-<!-- Add your site or application content here -->
-<p>Hello world! This is HTML5 Boilerplate.</p>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/assets/vendor/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/assets/vendor/components-font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="/assets/vendor/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/assets/vendor/AdminLTE/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="/assets/vendor/AdminLTE/dist/css/skins/skin-blue.min.css">
 
+    <script src="/assets/vendor/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="/assets/js/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="/assets/js/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+    <header class="main-header">
+        <nav class="navbar navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="../../index2.html" class="navbar-brand">
+                        <b>Admin</b>
+                        LTE
+                    </a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                </div>
+                <!-- Navbar Right Menu -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+    </header>
+    <!-- Full Width Column -->
+    <div class="content-wrapper">
+        <div class="container">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <div class="container">
+            <div class="pull-right hidden-xs">
+                <b>Version</b>
+                2.3.8
+            </div>
+            <strong>Copyright &copy; 2014-2016
+                <a href="http://almsaeedstudio.com">Almsaeed Studio</a>
+                    .
+            </strong>
+            All rights
+            reserved.
+        </div>
+        <!-- /.container -->
+    </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="/assets/vendor/jquery/dist/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/assets/vendor/jquery/dist/jquery.min.js"><\/script>')</script>
 <script src="/assets/vendor/html5-boilerplate/dist/js/plugins.min.js"></script>
-
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="/assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="/assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="/assets/vendor/fastclick/lib/fastclick.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/assets/vendor/AdminLTE/dist/js/app.min.js"></script>
 </body>
 </html>
