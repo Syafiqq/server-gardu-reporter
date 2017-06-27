a0d45df9e2db8aea51d99eaf3e27fa8e987344d4
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-27 07:26:39 +0700

    Add array serializable

---
+++ raw/application/model/util/CSerializable.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 24 June 2017, 6:57 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+interface CSerializable
+{
+    /**
+     * @return array
+     */
+    public function cSerialize(): array;
+}
+
+?>
