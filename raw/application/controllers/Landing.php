09a37edf2916531b31f612e80ce7ca892ddb1103
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-27 20:26:51 +0700

    Populate lang data to view

--- raw/application/controllers/Landing.php
+++ raw/application/controllers/Landing.php
@@ -8,6 +8,11 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Class Landing
+ * @property CI_Config config
+ * @property CI_Lang lang
+ */
 class Landing extends CI_Controller
 {
     /**
@@ -26,19 +31,44 @@ class Landing extends CI_Controller
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
 
+    /**
+     * @var string $language
+     * @var string $lang_prefix
+     */
+    private $language;
+    private $lang_prefix;
+
     public function __construct()
     {
         parent::__construct();
         // Your own constructor code
+        $this->load->helper('cookie');
+
+        $this->language = (!empty(get_cookie('common_language')) ?: $this->config->item('language'));
     }
 
     public function index()
     {
         $this->load->helper('url');
+        $this->lang_prefix = 'landing_index_client_common_layout_';
 
+        $this->lang->load('layout/landing/index/landing_index_client_common_layout', $this->language);
+        $this->lang->load('common', $this->language);
+
+        $string = [];
         $meta = [];
+
+        $string['title'] = $this->lang->line('common_title');
+        $string['table_header_no'] = $this->lang->line("{$this->lang_prefix}table_header_no");
+        $string['table_header_substation'] = $this->lang->line("{$this->lang_prefix}table_header_substation");
+        $string['table_header_current'] = $this->lang->line("{$this->lang_prefix}table_header_current");
+        $string['table_header_voltage'] = $this->lang->line("{$this->lang_prefix}table_header_voltage");
+        $string['table_header_location'] = $this->lang->line("{$this->lang_prefix}table_header_location");
+
         $meta['retriever'] = site_url('api/find/');
-        $this->load->view('landing/index/landing_index_client_common_layout', compact('meta'));
+        $meta['datatable_lang'] = base_url($this->lang->line('common_datatable_lang'));
+
+        $this->load->view('landing/index/landing_index_client_common_layout', compact('meta', 'string'));
     }
 }
 
