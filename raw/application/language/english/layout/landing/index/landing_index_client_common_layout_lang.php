9aa59828e6cf05831aabab2a6b83bd4c93935411
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-27 20:25:48 +0700

    Add landing index client common layout lang data

---
+++ raw/application/language/english/layout/landing/index/landing_index_client_common_layout_lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 27 June 2017, 7:18 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+$lang['landing_index_client_common_layout_table_header_no'] = 'No';
+$lang['landing_index_client_common_layout_table_header_substation'] = 'Substation';
+$lang['landing_index_client_common_layout_table_header_current'] = 'Current';
+$lang['landing_index_client_common_layout_table_header_voltage'] = 'Voltage';
+$lang['landing_index_client_common_layout_table_header_location'] = 'Location';
+
+?>
+
