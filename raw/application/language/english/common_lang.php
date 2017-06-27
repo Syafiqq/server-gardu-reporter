610b22453be3ff95c78bf347b60e44cc112adb2b
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-27 20:25:32 +0700

    Add common lang data

---
+++ raw/application/language/english/common_lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 27 June 2017, 7:24 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+$lang['common_title'] = 'Gardu Reporter';
+$lang['common_datatable_lang'] = '/assets/json/datatable/1.10.15/i18n/English.min.json';
+
+?>
+
+
