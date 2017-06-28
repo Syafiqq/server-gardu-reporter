506997b8326335e15931fd5066bd3aa5e4f03677
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-28 10:34:45 +0700

    Add common table header lang

---
+++ raw/application/language/english/common/table/common_table_header_lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 28 June 2017, 10:06 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+$lang['common_table_header_no'] = 'No';
+$lang['common_table_header_substation'] = 'Substation';
+$lang['common_table_header_current'] = 'Current';
+$lang['common_table_header_voltage'] = 'Voltage';
+$lang['common_table_header_location'] = 'Location';
+
+?>
+
+
