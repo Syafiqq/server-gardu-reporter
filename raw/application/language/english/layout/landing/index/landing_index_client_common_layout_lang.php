06a4df29918a7731109fdec4049e802ec44b10d9
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-28 10:33:10 +0700

    Move table header to common table header file

--- raw/application/language/english/layout/landing/index/landing_index_client_common_layout_lang.php
+++ raw/application/language/english/layout/landing/index/landing_index_client_common_layout_lang.php
@@ -7,11 +7,6 @@
  * Github       : syafiqq
  */
 
-$lang['landing_index_client_common_layout_table_header_no'] = 'No';
-$lang['landing_index_client_common_layout_table_header_substation'] = 'Substation';
-$lang['landing_index_client_common_layout_table_header_current'] = 'Current';
-$lang['landing_index_client_common_layout_table_header_voltage'] = 'Voltage';
-$lang['landing_index_client_common_layout_table_header_location'] = 'Location';
 
 ?>
 
