79e018745413446ea1e0ef5a83b768030be08793
Syafiqq <syafiq.rezpector@gmail.com>
2017-06-30 05:00:04 +0700

    Add admin auth login common layout

---
+++ raw/application/language/english/layout/admin/auth/login/admin_auth_login_common_layout_lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 28 June 2017, 11:13 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+?>
