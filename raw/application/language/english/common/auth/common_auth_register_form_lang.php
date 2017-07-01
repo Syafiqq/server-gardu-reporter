42b6ed8ac0c9882e95bee96d3f6d92acf39951d0
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-01 23:24:02 +0700

    Add common auth register form language

---
+++ raw/application/language/english/common/auth/common_auth_register_form_lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 01 July 2017, 11:13 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+$lang['common_auth_register_form_username_label'] = 'Username';
+$lang['common_auth_register_form_email_label'] = 'Email';
+$lang['common_auth_register_form_password_label'] = 'Password';
+$lang['common_auth_register_form_password_confirmation_label'] = 'Confirm Password';
+
+?>
+
