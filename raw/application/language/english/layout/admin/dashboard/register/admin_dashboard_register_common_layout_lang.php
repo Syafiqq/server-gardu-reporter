030845e06e84139fd31ddfdfa6b02e44c3fff8dc
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-01 23:24:17 +0700

    Add some resource lang

--- raw/application/language/english/layout/admin/dashboard/register/admin_dashboard_register_common_layout_lang.php
+++ raw/application/language/english/layout/admin/dashboard/register/admin_dashboard_register_common_layout_lang.php
@@ -5,4 +5,12 @@
  * Date / Time  : 30 June 2017, 4:19 PM.
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
- */ ?>
+ */
+
+$lang['common_dashboard_register_common_layout_register_client_username_placeholder'] = 'Put the client username here!';
+$lang['common_dashboard_register_common_layout_register_client_email_placeholder'] = 'Put the client email here!';
+$lang['common_dashboard_register_common_layout_register_client_password_placeholder'] = 'Put the client password here!';
+$lang['common_dashboard_register_common_layout_register_client_password_confirmation_placeholder'] = 'Confirm the client password here!';
+
+?>
+
