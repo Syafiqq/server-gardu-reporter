f5cda86d45197133d0308864499c1482d5e49562
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-01 22:12:17 +0700

    Add admin dashboard controller Add register route

--- raw/application/controllers/admin/Dashboard.php
+++ raw/application/controllers/admin/Dashboard.php
@@ -8,6 +8,10 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * @property CI_Config config
+ * @property CI_Lang lang
+ */
 class Dashboard extends CI_Controller
 {
     /**
@@ -26,15 +30,45 @@ class Dashboard extends CI_Controller
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
 
+    /**
+     * @var string $language
+     * @var string $country
+     * @var string $lang_prefix
+     */
+    private $language;
+    private $country;
+    private $lang_prefix;
+
     public function __construct()
     {
         parent::__construct();
         // Your own constructor code
+        $this->load->helper('cookie');
+
+        $this->language = empty($this->language = get_cookie('common_language')) ? $this->config->item('language') : $this->language;
+        $this->country = empty($this->country = get_cookie('common_country')) ? $this->config->item('country') : $this->country;
     }
 
     public function register()
     {
-        $this->load->view('admin/dashboard/register/admin_dashboard_register_common_layout');
+        $this->load->helper(['url', 'i18n']);
+        $this->lang_prefix = 'admin_dashboard_register';
+
+        $this->lang->load("layout/admin/dashboard/register/{$this->lang_prefix}_common_layout", $this->language);
+        $this->lang->load('common/auth/common_auth_common', $this->language);
+        $this->lang->load('common', $this->language);
+
+        $string = [];
+        $meta = [];
+        $data = [];
+
+        $string['title'] = $this->lang->line('common_title');
+        $string['client_register'] = $this->lang->line('common_auth_common_register');
+
+        $data['meta']['i18n']['country'] = empty($data['meta']['i18n']['country'] = i18nGetCountryCode($this->country)) ? 'US' : $data['meta']['i18n']['country'];
+        $data['meta']['i18n']['language'] = empty($data['meta']['i18n']['language'] = i18nGetLanguageCode($this->language)) ? 'en' : $data['meta']['i18n']['language'];
+
+        $this->load->view("admin/dashboard/register/{$this->lang_prefix}_common_layout", compact('meta', 'string', 'data'));
     }
 }
 
