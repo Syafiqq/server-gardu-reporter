44fc0d74de5683c36bc3e406ff632f645031cb9d
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-01 22:11:47 +0700

    Add admin dashboard client register language

---
+++ raw/application/language/english/layout/admin/dashboard/register/admin_dashboard_register_common_layout_lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 30 June 2017, 4:19 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */ ?>
