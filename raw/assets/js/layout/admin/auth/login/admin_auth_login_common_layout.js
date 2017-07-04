20797136cd834312a2418b0698021413f63a7366
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-04 20:59:27 +0700

    Add initial form submit function

--- raw/assets/js/layout/admin/auth/login/admin_auth_login_common_layout.js
+++ raw/assets/js/layout/admin/auth/login/admin_auth_login_common_layout.js
@@ -10,6 +10,21 @@
 {
     $(function ()
     {
+        $('form#login').on('submit', function (event)
+        {
+            event.preventDefault();
+
+            var input = $(this).serializeObject();
+
+            input['remember_me'] = $(this).find('input[type=checkbox][name=remember_me]').prop('checked');
+            if ((input['remember_me'] === undefined) || (input['remember_me'] === null))
+            {
+                input['remember_me'] = false;
+            }
+
+            console.log(input);
+        });
+
         $('input').iCheck({
             checkboxClass: 'icheckbox_square-blue',
             radioClass: 'iradio_square-blue',
