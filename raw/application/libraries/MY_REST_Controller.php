f87613386a98c1193a14f17ff1432003ec5bc9ec
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-05 13:45:56 +0700

    Extent REST_Controller

---
+++ raw/application/libraries/MY_REST_Controller.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 05 July 2017, 8:06 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace Restserver\Libraries;
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once APPPATH . '/libraries/REST_Controller.php';
+
+class MY_REST_Controller extends REST_Controller
+{
+    public function __construct($config = 'rest')
+    {
+        parent::__construct($config);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed|null $default_value
+     * @param bool $xss_clean
+     * @return mixed|array|null
+     */
+    public function postOrDefault($key = null, $default_value = null, $xss_clean = null)
+    {
+        $value = parent::post($key, $xss_clean);
+
+        if (!$value)
+        {
+            $value = $default_value;
+        }
+
+        return $value;
+    }
+}
+
+?>
