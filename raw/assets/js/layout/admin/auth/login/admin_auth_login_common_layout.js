9027b11d4176921962ce36273a85babba69a5665
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-05 19:01:48 +0700

    Reformat alert spawner Add flash data notifier

--- raw/assets/js/layout/admin/auth/login/admin_auth_login_common_layout.js
+++ raw/assets/js/layout/admin/auth/login/admin_auth_login_common_layout.js
@@ -10,6 +10,11 @@
 {
     $(function ()
     {
+        NProgress.configure({
+            showSpinner: false,
+            template: '<div class="bar" role="bar" style="background-color: red"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+        });
+
         $('form#login').on('submit', function (event)
         {
             event.preventDefault();
@@ -20,22 +25,63 @@
             input['remember_me'] = $(this).find('input[type=checkbox][name=remember_me]').prop('checked');
             if ((input['remember_me'] === undefined) || (input['remember_me'] === null))
             {
-                input['remember_me'] = false;
+                input['remember_me'] = 0;
+            }
+            else
+            {
+                input['remember_me'] = input['remember_me'] ? 1 : 0;
             }
 
+            NProgress.start();
             $.post(
                 form.attr('action'),
                 input,
                 null,
                 'json')
-                .done(function (data)
+                .done(function (response)
+                {
+                    let status = ['danger', 'info', 'warning', 'success'];
+                    let type = ['login', 'validation'];
+                    if (response['data'] !== undefined)
+                    {
+                        if (response['data']['message'] !== undefined)
+                        {
+                            for (let i = -1, is = type.length; ++i < is;)
+                            {
+                                if (response['data']['message'][type[i]] !== undefined)
+                                {
+                                    for (let j = -1, js = status.length; ++j < js;)
+                                    {
+                                        if (response['data']['message'][type[i]][status[j]] !== undefined)
+                                        {
+                                            let template = '<div class="alert alert-' + status[j] + ' alert-dismissible">'
+                                                + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'
+                                                + '<ul>';
+                                            for (let k = -1, ks = response['data']['message'][type[i]][status[j]].length; ++k < ks;)
+                                            {
+                                                template += '<li>' + response['data']['message'][type[i]][status[j]][k] + '</li>'
+                                            }
+                                            template += '</ul>'
+                                                + '</div>';
+                                            $("div#form-message-container").empty().append(template);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if (response['data']['redirect'] !== undefined)
+                        {
+                            location.href = response['data']['redirect'];
+                        }
+                    }
+                })
+                .fail(function ()
                 {
                 })
-                .fail(function (data)
+                .always(function ()
                 {
-                })
-                .always(function (data)
-                {
+                    NProgress.done();
                 });
         });
 
@@ -44,6 +90,18 @@
             radioClass: 'iradio_square-blue',
             increaseArea: '20%' // optional
         });
+
+        if ((sessionFlashdata !== undefined) && (sessionFlashdata !== null))
+        {
+            for (let i = -1, is = sessionFlashdata.length; ++i < is;)
+            {
+                $.notify({
+                    message: sessionFlashdata[i]
+                }, {
+                    type: 'info'
+                });
+            }
+        }
     });
     /*
      * Run right away
