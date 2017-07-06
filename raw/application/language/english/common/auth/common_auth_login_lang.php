01d9d7200d0875eab118fbc2bf7b5770dc32f5dd
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-06 07:39:52 +0700

    Move form component to proper place

--- raw/application/language/english/common/auth/common_auth_login_lang.php
+++ raw/application/language/english/common/auth/common_auth_login_lang.php
@@ -9,8 +9,5 @@
 
 /** @var array $lang */
 $lang['common_auth_login_login_box_message'] = '%s in to start your session';
-$lang['common_auth_login_email_label'] = 'Email';
-$lang['common_auth_login_password_label'] = 'Password';
-$lang['common_auth_login_remember_me_label'] = 'Remember Me';
 
 ?>
