5ca9d2cf017d9040f90f16a8144f16c2340ad603
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 17:52:36 +0700

    Finishing login api

--- raw/application/controllers/api/Mobile.php
+++ raw/application/controllers/api/Mobile.php
@@ -54,25 +54,57 @@ class Mobile extends \Restserver\Libraries\MY_REST_Controller
 
         $this->load->database();
         /** @noinspection PhpParamsInspection */
-        $this->load->library(['ion_auth']);
+        $this->load->library(['ion_auth', 'form_validation']);
         $this->load->helper(['url']);
 
-        $this->authGroup = 'member';
-
-        $this->language = empty($this->language = get_cookie('common_language')) ? $this->config->item('language') : $this->language;
+        $this->authGroup = 'members';
     }
 
     public function login_post()
     {
+        $this->language = $this->getOrDefault('lang', $this->config->item('language'));
+
         /** @var array $response */
         $response = [];
+        $response['data']['status'] = 0;
+
         if (!empty($_SERVER['HTTP_X_ACCESS_PERMISSION']))
         {
             if (strcmp(strtolower($_SERVER['HTTP_X_ACCESS_PERMISSION']), $this->config->item('non_csrf_permission')) === 0)
             {
                 if (strcmp(strtolower($this->postOrDefault('guard', null)), $this->config->item('non_csrf_guard')) === 0)
                 {
+                    $this->lang->load(['common/auth/common_auth_login_form', 'auth'], $this->language);
 
+                    /** @var array $data */
+                    $data = [];
+
+                    $data['identity'] = $this->postOrDefault('identity', null);
+                    $data['password'] = $this->postOrDefault('password', null);
+
+                    $this->form_validation->set_rules('identity', $this->lang->line('common_auth_login_form_email_label'), 'required|valid_email');
+                    $this->form_validation->set_rules('password', $this->lang->line('common_auth_login_form_password_label'), 'required');
+
+                    $this->form_validation->set_data($data);
+                    if ($this->form_validation->run() == true)
+                    {
+                        $remember = false;
+
+                        if ($this->ion_auth->login_for_api($data['identity'], $data['password'], $this->authGroup, $remember))
+                        {
+                            $response['data']['token'] = $this->ion_auth->getApiToken();
+                            $response['data']['status'] = 1;
+                            $response['data']['message'] = array_merge([], is_array($this->ion_auth->messages()) ? $this->ion_auth->messages() : explode(PHP_EOL, trim($this->ion_auth->messages())));
+                        }
+                        else
+                        {
+                            $response['data']['message'] = array_merge([], is_array($this->ion_auth->errors()) ? $this->ion_auth->errors() : explode(PHP_EOL, trim($this->ion_auth->errors())));
+                        }
+                    }
+                    else
+                    {
+                        $response['data']['message'] = $this->validation_errors();
+                    }
                 }
             }
         }
