f677e35aa16c08a7e2e0626dadb0fc4594f69448
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 17:51:31 +0700

    Extend ion auth lang

--- raw/application/language/english/ion_auth_extended_lang.php
+++ raw/application/language/english/ion_auth_extended_lang.php
@@ -11,6 +11,8 @@
 // Login / Logout
 $lang['login_already_active'] = 'Your account is already active';
 $lang['client_account_creation_forbidden'] = 'You are not allowed to execute this operation';
+$lang['deactivate_previous_token_successful'] = 'Previous token successfully deactivated';
+$lang['register_new_token_successful'] = 'Create new Token Successful';
 
 
 ?>
