fd54be8d145d44ec576482fae3e675c3ff02cadb
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-07 15:22:07 +0700

    Add api default route

---
+++ raw/application/controllers/api/Default_route.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 07 July 2017, 2:20 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Default_route extends CI_Controller
+{
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *        http://example.com/index.php/welcome
+     *    - or -
+     *        http://example.com/index.php/welcome/index
+     *    - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+
+    public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+    }
+
+    public function index()
+    {
+
+    }
+}
+
+?>
