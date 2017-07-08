2542e2a270bbecf50bae5f1456d5d1f27dffc04a
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-08 09:42:06 +0700

    Add find method

--- raw/application/controllers/api/Default_route.php
+++ raw/application/controllers/api/Default_route.php
@@ -8,7 +8,18 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Default_route extends CI_Controller
+
+require_once APPPATH . '/dao/DAOReport.php';
+require_once APPPATH . '/model/ModelReport.php';
+require_once APPPATH . '/model/ModelLocation.php';
+require_once APPPATH . '/libraries/REST_Controller.php';
+
+/**
+ * @property MLocation mLocation
+ * @property MReport mReport
+ * @property CI_Input input
+ */
+class Default_route extends \Restserver\Libraries\REST_Controller
 {
     /**
      * Index Page for this controller.
@@ -32,9 +43,29 @@ class Default_route extends CI_Controller
         // Your own constructor code
     }
 
-    public function index()
+    public function find_get()
     {
+        $get = $this->get();
+        if (!isset($get['id']))
+        {
+            $get['id'] = null;
+        }
 
+        $this->load->database();
+
+        $this->load->model('mReport');
+        $reports = [];
+
+        /** @var DAOReport $report */
+        foreach ($this->mReport->find($get['id']) as $report)
+        {
+            array_push($reports, $report->cSerialize());
+        }
+
+        $this->response([
+            'status' => \Restserver\Libraries\REST_Controller::HTTP_OK,
+            'data' => ['reports' => $reports]
+        ], \Restserver\Libraries\REST_Controller::HTTP_OK);
     }
 }
 
