ff70b5908dcaf1e8d26d12d82b4e183aed6bf679
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-08 07:55:36 +0700

    Add common token

---
+++ raw/application/language/english/common/auth/common_auth_token_form_lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 08 July 2017, 5:51 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+/** @var array $lang */
+$lang['common_auth_token_form_token_label'] = 'Token';
+
+?>
+
