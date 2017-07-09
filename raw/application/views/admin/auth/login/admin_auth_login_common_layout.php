a82d90b1bc8e11118271654b9672ff7f4bc6370d
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-09 07:58:01 +0700

    Remove email and password default value

--- raw/application/views/admin/auth/login/admin_auth_login_common_layout.php
+++ raw/application/views/admin/auth/login/admin_auth_login_common_layout.php
@@ -89,11 +89,11 @@ if (!isset($data))
             echo @form_hidden('redirector', $data['session']['redirector']);
         } ?>
         <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="<?php echo @$string['login_identity'] ?>" name="identity" value="admin@admin.com">
+            <input type="text" class="form-control" placeholder="<?php echo @$string['login_identity'] ?>" name="identity">
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="<?php echo @$string['login_password'] ?>" name="password" value="password">
+            <input type="password" class="form-control" placeholder="<?php echo @$string['login_password'] ?>" name="password">
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
