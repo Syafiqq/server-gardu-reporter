88f978095d29e34f85a08d9368a675dbb99fd72e
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-17 18:35:20 +0700

    Extent lang

--- raw/application/language/english/layout/management/user/admin/management_user_admin_common_layout_lang.php
+++ raw/application/language/english/layout/management/user/admin/management_user_admin_common_layout_lang.php
@@ -11,5 +11,9 @@
 $lang['management_user_admin_common_layout_page_title'] = 'User Management';
 $lang['management_user_admin_common_layout_forbidden_access'] = 'You Are Not Allowed Access This Page';
 $lang['management_user_admin_common_layout_forbidden_access_auth_redirection'] = 'Please Login First.';
+$lang['management_user_admin_common_layout_table_header_email'] = 'Email';
+$lang['management_user_admin_common_layout_table_header_username'] = 'Username';
+$lang['management_user_admin_common_layout_table_header_role'] = 'Role';
+$lang['management_user_admin_common_layout_table_header_option'] = 'Option';
 ?>
 
