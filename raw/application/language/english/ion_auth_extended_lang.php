43132086d13e94303b689e08b96149d65b60b3c0
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-18 19:12:47 +0700

    Add role checking

--- raw/application/language/english/ion_auth_extended_lang.php
+++ raw/application/language/english/ion_auth_extended_lang.php
@@ -18,6 +18,7 @@ $lang['insufficient_data'] = 'Insufficient data when execute this operation';
 $lang['success_remove_from_group'] = 'User has been successfully remove from group';
 $lang['failed_remove_from_group'] = 'Cannot remove user from group';
 $lang['deactivate_previous_token_successful'] = 'Previous token successfully deactivated';
+$lang['check_role_undefined'] = 'Level Not Defined';
 $lang['register_new_token_successful'] = 'Create new Token Successful';
 $lang['check_token_unsuccessful'] = 'Token Invalid';
 $lang['check_token_successful'] = 'Token Valid';
