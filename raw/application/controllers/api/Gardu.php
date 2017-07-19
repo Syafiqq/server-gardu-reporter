919a52d18692f35e2984efa8b1bd71bf9151b83c
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-19 08:58:59 +0700

    Add update function Remodel is_id_exists

--- raw/application/controllers/api/Gardu.php
+++ raw/application/controllers/api/Gardu.php
@@ -202,6 +202,7 @@ class Gardu extends \Restserver\Libraries\MY_REST_Controller
             $this->lang->load('layout/gardu/induk/gardu_induk_common', $this->language);
 
             $this->callback_request['_id_existence_check'] = true;
+            $this->callback_request['_id_existence_check_need_exists'] = false;
 
             $this->form_validation->set_data($data);
 
@@ -243,6 +244,72 @@ class Gardu extends \Restserver\Libraries\MY_REST_Controller
     }
 
     /**
+     *
+     */
+    public function induk_update_patch()
+    {
+        $this->load->library('session');
+        /** @var array $response */
+        $response = [];
+
+        if ($this->ion_auth->logged_in() && $this->ion_auth->is_admin())
+        {
+            $this->load->library('form_validation');
+
+            /** @var array $data
+             * @var string $identity_column
+             */
+            $data = [];
+
+            $data['id_tb_gardu_induk'] = $this->patchOrDefault('id', null);
+            $data['nama_gardu_induk'] = $this->patchOrDefault('name', null);
+
+            $this->lang->load('layout/gardu/induk/gardu_induk_common', $this->language);
+
+            $this->callback_request['_id_existence_check'] = true;
+            $this->callback_request['_id_existence_check_need_exists'] = true;
+
+            $this->form_validation->set_data($data);
+
+            // validate form input
+            $this->form_validation->set_rules('id_tb_gardu_induk', $this->lang->line('gardu_induk_common_form_id_label'), 'required|callback__id_existence_check');
+            $this->form_validation->set_rules('nama_gardu_induk', $this->lang->line('gardu_induk_common_form_name_label'), 'required');
+
+            $isValid = $this->form_validation->run();
+            $isValid &= (($data['id_tb_gardu_induk'] = intval($data['id_tb_gardu_induk'])) != 0);
+            if ($isValid)
+            {
+                $this->load->model('model_gardu_induk', 'mgi');
+                $id = $data['id_tb_gardu_induk'];
+                unset($data['id_tb_gardu_induk']);
+                if ($this->mgi->update($id, $data))
+                {
+                    $response['data']['status'] = 1;
+                    $response['data']['message']['message']['update']['success'] = [$this->lang->line('gardu_induk_common_update_success')];
+                    $response['data']['message']['notify']['update']['success'] = $response['data']['message']['message']['update']['success'];
+                }
+                else
+                {
+                    $response['data']['status'] = 0;
+                    $response['data']['message']['message']['update']['danger'] = [$this->lang->line('gardu_induk_common_update_failed')];
+                }
+            }
+            else
+            {
+                $response['data']['status'] = 0;
+                $response['data']['message']['message']['validation']['info'] = $this->validation_errors();
+            }
+        }
+        else
+        {
+            $response['data']['message']['message']['update']['info'] = [$this->lang->line('gardu_induk_common_manipulation_forbidden')];
+        }
+
+        $response['status'] = \Restserver\Libraries\REST_Controller::HTTP_OK;
+        $this->response($response, $response['status']);
+    }
+
+    /**
      * @param int $id
      * @return bool|mixed
      */
@@ -257,19 +324,37 @@ class Gardu extends \Restserver\Libraries\MY_REST_Controller
         else
         {
             unset($this->callback_request['_id_existence_check']);
+            $need_exist = isset($this->callback_request['_id_existence_check_need_exists']) ? $this->callback_request['_id_existence_check_need_exists'] : false;
             $id = intval($id);
 
             $this->load->model('model_gardu_induk', 'mgi');
             if ($this->mgi->id_check($id))
             {
-                $this->lang->load('layout/gardu/induk/gardu_induk_common', $this->language);
-                $this->form_validation->set_message('_id_existence_check', $this->lang->line('gardu_induk_common_form_id_exists_error'));
+                if ($need_exist)
+                {
+                    return true;
+                }
+                else
+                {
+                    $this->lang->load('layout/gardu/induk/gardu_induk_common', $this->language);
+                    $this->form_validation->set_message('_id_existence_check', $this->lang->line('gardu_induk_common_form_id_exists_error'));
 
-                return false;
+                    return false;
+                }
             }
             else
             {
-                return true;
+                if ($need_exist)
+                {
+                    $this->lang->load('layout/gardu/induk/gardu_induk_common', $this->language);
+                    $this->form_validation->set_message('_id_existence_check', $this->lang->line('gardu_induk_common_form_id_not_exists_error'));
+
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
             }
         }
     }
