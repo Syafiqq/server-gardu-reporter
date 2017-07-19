adac3805df87ea34bcda89f85eee1a04c42d0cb9
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-19 18:31:04 +0700

    Add gardu penyulang funcgion

---
+++ raw/application/language/english/layout/gardu/penyulang/admin/gardu_penyulang_admin_common_layout_lang.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 19 July 2017, 5:20 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+/** @var $lang array */
+$lang['gardu_penyulang_admin_common_layout_page_title'] = 'Data Penyulang';
+$lang['gardu_penyulang_admin_common_layout_add_new_item'] = 'Tambah Data';
+$lang['gardu_penyulang_admin_common_layout_forbidden_access'] = 'You Are Not Allowed Access This Page';
+$lang['gardu_penyulang_admin_common_layout_forbidden_access_auth_redirection'] = 'Please Login First.';
+$lang['gardu_penyulang_admin_common_layout_table_header_code'] = 'ID';
+$lang['gardu_penyulang_admin_common_layout_table_header_name'] = 'Name';
+$lang['gardu_penyulang_admin_common_layout_table_header_option'] = 'Option';
+$lang['gardu_penyulang_admin_common_layout_table_content_cell_edit'] = 'Edit';
+$lang['gardu_penyulang_admin_common_layout_table_content_cell_delete'] = 'Delete';
+$lang['gardu_penyulang_admin_common_layout_register_new_item_title'] = 'Tambah Data Baru';
+$lang['gardu_penyulang_admin_common_layout_register_new_item_button'] = 'Simpan';
+$lang['gardu_penyulang_admin_common_layout_item_creation_form_id_label'] = 'ID Gardu Penyulang';
+$lang['gardu_penyulang_admin_common_layout_item_creation_form_name_label'] = 'Nama Penyulang';
+$lang['gardu_penyulang_admin_common_layout_item_creation_form_id_placeholder'] = 'Masukkan ID Gardu Penyulang';
+$lang['gardu_penyulang_admin_common_layout_item_creation_form_name_placeholder'] = 'Masukkan Nama Penyulang';
+$lang['gardu_penyulang_admin_common_layout_update_existing_item_title'] = 'Perbarui Data Penyulang';
+$lang['gardu_penyulang_admin_common_layout_update_existing_item_button'] = 'Simpan';
+?>
