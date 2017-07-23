cf9b8d69a35697534540c5f9749a82d9a0fc2738
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-23 15:13:21 +0700

    Fix Table Width

--- raw/application/views/gardu/index/admin/gardu_index_admin_common_layout.php
+++ raw/application/views/gardu/index/admin/gardu_index_admin_common_layout.php
@@ -178,11 +178,11 @@ if (!isset($view))
                             <table id="table_gardu_index" class="table table-bordered table-hover table-striped">
                                 <thead>
                                 <tr>
-                                    <th><?php echo @$string['table_header_induk_id'] ?></th>
-                                    <th><?php echo @$string['table_header_penyulang_id'] ?></th>
-                                    <th><?php echo @$string['table_header_no'] ?></th>
+                                    <th width="15%"><?php echo @$string['table_header_induk_id'] ?></th>
+                                    <th width="15%"><?php echo @$string['table_header_penyulang_id'] ?></th>
+                                    <th width="20%"><?php echo @$string['table_header_no'] ?></th>
                                     <th><?php echo @$string['table_header_location'] ?></th>
-                                    <th><?php echo @$string['table_header_option'] ?></th>
+                                    <th width="15%"><?php echo @$string['table_header_option'] ?></th>
                                 </tr>
                                 </thead>
                                 <tbody>
