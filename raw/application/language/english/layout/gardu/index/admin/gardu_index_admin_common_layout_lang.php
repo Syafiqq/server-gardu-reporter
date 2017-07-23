4fecdc9003dec2f9327947ab289db76f3a15f6c4
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-23 14:24:26 +0700

    Fix renaming lang component

--- raw/application/language/english/layout/gardu/index/admin/gardu_index_admin_common_layout_lang.php
+++ raw/application/language/english/layout/gardu/index/admin/gardu_index_admin_common_layout_lang.php
@@ -8,22 +8,22 @@
  */
 
 /** @var $lang array */
-$lang['gardu_penyulang_admin_common_layout_page_title'] = 'Data Penyulang';
-$lang['gardu_penyulang_admin_common_layout_add_new_item'] = 'Tambah Data';
-$lang['gardu_penyulang_admin_common_layout_forbidden_access'] = 'You Are Not Allowed Access This Page';
-$lang['gardu_penyulang_admin_common_layout_forbidden_access_auth_redirection'] = 'Please Login First.';
-$lang['gardu_penyulang_admin_common_layout_table_header_code'] = 'ID';
-$lang['gardu_penyulang_admin_common_layout_table_header_name'] = 'Name';
-$lang['gardu_penyulang_admin_common_layout_table_header_option'] = 'Option';
-$lang['gardu_penyulang_admin_common_layout_table_content_cell_edit'] = 'Edit';
-$lang['gardu_penyulang_admin_common_layout_table_content_cell_delete'] = 'Delete';
-$lang['gardu_penyulang_admin_common_layout_register_new_item_title'] = 'Tambah Data Baru';
-$lang['gardu_penyulang_admin_common_layout_register_new_item_button'] = 'Simpan';
-$lang['gardu_penyulang_admin_common_layout_item_creation_form_id_label'] = 'ID Gardu Penyulang';
-$lang['gardu_penyulang_admin_common_layout_item_creation_form_name_label'] = 'Nama Penyulang';
-$lang['gardu_penyulang_admin_common_layout_item_creation_form_id_placeholder'] = 'Masukkan ID Gardu Penyulang';
-$lang['gardu_penyulang_admin_common_layout_item_creation_form_name_placeholder'] = 'Masukkan Nama Penyulang';
-$lang['gardu_penyulang_admin_common_layout_update_existing_item_title'] = 'Perbarui Data Penyulang';
-$lang['gardu_penyulang_admin_common_layout_update_existing_item_button'] = 'Simpan';
+$lang['gardu_index_admin_common_layout_page_title'] = 'Data Gardu';
+$lang['gardu_index_admin_common_layout_add_new_item'] = 'Tambah Data';
+$lang['gardu_index_admin_common_layout_forbidden_access'] = 'You Are Not Allowed Access This Page';
+$lang['gardu_index_admin_common_layout_forbidden_access_auth_redirection'] = 'Please Login First.';
+$lang['gardu_index_admin_common_layout_table_header_code'] = 'ID';
+$lang['gardu_index_admin_common_layout_table_header_name'] = 'Name';
+$lang['gardu_index_admin_common_layout_table_header_option'] = 'Option';
+$lang['gardu_index_admin_common_layout_table_content_cell_edit'] = 'Edit';
+$lang['gardu_index_admin_common_layout_table_content_cell_delete'] = 'Delete';
+$lang['gardu_index_admin_common_layout_register_new_item_title'] = 'Tambah Data Baru';
+$lang['gardu_index_admin_common_layout_register_new_item_button'] = 'Simpan';
+$lang['gardu_index_admin_common_layout_item_creation_form_id_label'] = 'ID Gardu';
+$lang['gardu_index_admin_common_layout_item_creation_form_name_label'] = 'Nama Gardu';
+$lang['gardu_index_admin_common_layout_item_creation_form_id_placeholder'] = 'Masukkan ID Gardu';
+$lang['gardu_index_admin_common_layout_item_creation_form_name_placeholder'] = 'Masukkan Nama Gardu';
+$lang['gardu_index_admin_common_layout_update_existing_item_title'] = 'Perbarui Data Gardu';
+$lang['gardu_index_admin_common_layout_update_existing_item_button'] = 'Simpan';
 ?>
 
