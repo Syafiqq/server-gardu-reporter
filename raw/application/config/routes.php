77c86cfecac689b5a48420143887215345e5ad68
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-23 14:25:40 +0700

    Fix path

--- raw/application/config/routes.php
+++ raw/application/config/routes.php
@@ -60,6 +60,10 @@ $route['api/gardu/penyulang/find'] = '/api/gardu/penyulang_find';
 $route['api/gardu/penyulang/register'] = '/api/gardu/penyulang_register';
 $route['api/gardu/penyulang/delete'] = '/api/gardu/penyulang_delete';
 $route['api/gardu/penyulang/update'] = '/api/gardu/penyulang_update';
+$route['api/gardu/index/find'] = '/api/gardu/index_find';
+$route['api/gardu/index/register'] = '/api/gardu/index_register';
+$route['api/gardu/index/delete'] = '/api/gardu/index_delete';
+$route['api/gardu/index/update'] = '/api/gardu/index_update';
 
 $route['api/mobile/(:any)'] = 'api/mobile/$1';
 $route['api/(:any)'] = 'api/default_route/$1';
