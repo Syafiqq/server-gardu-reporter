fef08f453131360ebe5859399dccbcb2046ee882
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-24 17:23:17 +0700

    Initial Phase

---
+++ raw/application/language/english/layout/gardu/index/detail/admin/gardu_index_detail_admin_common_layout_lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 24 July 2017, 5:12 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+
+/** @var $lang array */
+//Page Propertis======================================================================================================
+$lang['gardu_index_admin_common_layout_page_title'] = 'Data Gardu Trafo';
+$lang['gardu_index_admin_common_layout_add_new_item'] = 'Tambah Data';
+
+
+//Exception===========================================================================================================
+$lang['gardu_index_admin_common_layout_forbidden_access'] = 'You Are Not Allowed Access This Page';
+$lang['gardu_index_admin_common_layout_forbidden_access_auth_redirection'] = 'Please Login First.';
+
+//Table Header========================================================================================================
+$lang['gardu_index_admin_common_layout_table_header_induk_id'] = 'Gardu Induk';
+$lang['gardu_index_admin_common_layout_table_header_penyulang_id'] = 'Gardu Penyulang';
+$lang['gardu_index_admin_common_layout_table_header_no'] = 'No. Gardu';
+$lang['gardu_index_admin_common_layout_table_header_location'] = 'Lokasi';
+$lang['gardu_index_admin_common_layout_table_header_option'] = 'Option';
+
+$lang['gardu_index_admin_common_layout_table_content_cell_edit'] = 'Edit';
+$lang['gardu_index_admin_common_layout_table_content_cell_delete'] = 'Delete';
+$lang['gardu_index_admin_common_layout_table_content_cell_detail'] = 'Detail';
+
+//CRUD Modal==========================================================================================================
+$lang['gardu_index_admin_common_layout_register_new_item_title'] = 'Tambah Data Baru';
+$lang['gardu_index_admin_common_layout_register_new_item_button'] = 'Simpan';
+
+$lang['gardu_index_admin_common_layout_item_creation_form_induk_id_placeholder'] = 'Masukkan ID Gardu Induk';
+$lang['gardu_index_admin_common_layout_item_creation_form_penyulang_id_placeholder'] = 'Masukkan ID Gardu Penyulang';
+$lang['gardu_index_admin_common_layout_item_creation_form_jenis_placeholder'] = 'Masukkan Jenis Gardu';
+$lang['gardu_index_admin_common_layout_item_creation_form_no_placeholder'] = 'Masukkan No. Gardu';
+$lang['gardu_index_admin_common_layout_item_creation_form_lokasi_placeholder'] = 'Masukkan Alamat';
+$lang['gardu_index_admin_common_layout_item_creation_form_merk_placeholder'] = 'Masukkan Merk Trafo';
+$lang['gardu_index_admin_common_layout_item_creation_form_serial_placeholder'] = 'Masukkan No. Seri Trafo';
+$lang['gardu_index_admin_common_layout_item_creation_form_daya_placeholder'] = 'Masukkan Daya Trafo';
+$lang['gardu_index_admin_common_layout_item_creation_form_fasa_placeholder'] = 'Masukkan Fasa';
+$lang['gardu_index_admin_common_layout_item_creation_form_tap_placeholder'] = 'Masukkan Tap';
+$lang['gardu_index_admin_common_layout_item_creation_form_jurusan_placeholder'] = 'Masukkan Jumlah Jurusan';
+$lang['gardu_index_admin_common_layout_item_creation_form_lat_placeholder'] = 'Masukkan Koordinat Latitude';
+$lang['gardu_index_admin_common_layout_item_creation_form_long_placeholder'] = 'Masukkan Koordinat Longitude';
+
+$lang['gardu_index_admin_common_layout_item_manipulation_form_induk_id_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_induk_id_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_penyulang_id_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_penyulang_id_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_jenis_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_jenis_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_no_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_no_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_lokasi_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_lokasi_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_merk_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_merk_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_serial_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_serial_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_daya_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_daya_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_fasa_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_fasa_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_tap_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_tap_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_jurusan_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_jurusan_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_lat_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_lat_placeholder'];
+$lang['gardu_index_admin_common_layout_item_manipulation_form_long_placeholder'] = $lang['gardu_index_admin_common_layout_item_creation_form_long_placeholder'];
+
+$lang['gardu_index_admin_common_layout_update_existing_item_title'] = 'Perbarui Data Gardu';
+$lang['gardu_index_admin_common_layout_update_existing_item_button'] = 'Simpan';
+$lang['gardu_index_admin_common_layout_update_existing_item_detail'] = 'Detail';
+
+?>
