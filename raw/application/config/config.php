40afa7dfe64f771b19c4210203c5241a8461e3a5
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-25 20:27:19 +0700

    Change to https

--- raw/application/config/config.php
+++ raw/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://freelancer.gardu.reporter.app/';
+$config['base_url'] = 'https://freelancer.gardu.reporter.app/';
 
 /*
 |--------------------------------------------------------------------------
