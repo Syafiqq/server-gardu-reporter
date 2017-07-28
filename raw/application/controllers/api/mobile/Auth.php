93c56786efa3d5aee4e4d4d68275ebfb93bed96b
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-29 06:38:06 +0700

    Add mobile auth

---
+++ raw/application/controllers/api/mobile/Auth.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 29 July 2017, 6:35 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once APPPATH . '/libraries/MY_REST_Controller.php';
+
+
+/**
+ * Class Auth
+ * @property CI_Form_validation form_validation
+ * @property CI_Lang lang
+ * @property CI_Loader load
+ * @property CI_Config config
+ * @property CI_Security security
+ * @property Ion_auth ion_auth
+ * @property array data
+ * @property CI_Input input
+ * @property CI_Session session
+ */
+class Auth extends \Restserver\Libraries\MY_REST_Controller
+{
+    /**
+     * @var int
+     */
+    private $clientGroup;
+    /**
+     * @var mixed|null|string
+     */
+    private $language;
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *        http://example.com/index.php/welcome
+     *    - or -
+     *        http://example.com/index.php/welcome/index
+     *    - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+
+        $this->load->database();
+        /** @noinspection PhpParamsInspection */
+        $this->load->library(['ion_auth', 'form_validation']);
+        $this->load->helper(['url', 'language']);
+
+        $this->clientGroup = 2;
+
+        $this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+        $this->language = empty($this->language = get_cookie('common_language')) ? $this->config->item('language') : $this->language;
+    }
+
+    /**
+     *
+     */
+    public function login_post($group = 'admin')
+    {
+        $this->load->library('session');
+        /** @var array $response */
+        $response = [];
+
+        if ($this->ion_auth->logged_in())
+        {
+            $this->lang->load('ion_auth_extended', $this->language);
+
+            $response['data']['message']['login']['info'] = $this->lang->line('login_already_active');
+        }
+        else
+        {
+            /** @var array $data */
+            $data = [];
+
+            $data['identity'] = $this->postOrDefault('identity', null);
+            $data['password'] = $this->postOrDefault('password', null);
+
+            $this->lang->load(['common/auth/common_auth_login_form', 'auth'], $this->language);
+            $this->form_validation->set_data($data);
+
+            $this->form_validation->set_rules('identity', $this->lang->line('common_auth_login_form_email_label'), 'required|valid_email');
+            $this->form_validation->set_rules('password', $this->lang->line('common_auth_login_form_password_label'), 'required');
+
+            if ($this->form_validation->run() == true)
+            {
+                $remember = boolval(($this->postOrDefault('remember_me', false)));
+
+                if ($this->ion_auth->login_and_check($data['identity'], $data['password'], $group, $remember))
+                {
+                    $response['data']['redirect'] = $this->postOrDefault('redirector', site_url('/dashboard'));
+                    $response['data']['status'] = 1;
+                    $flashdata = array_merge([], explode(PHP_EOL, trim($this->ion_auth->messages())));
+                    $this->session->set_flashdata(['flashdata' => ['message' => $flashdata]]);
+                }
+                else
+                {
+                    $response['data']['status'] = 0;
+                    $response['data']['message']['login']['danger'] = [];
+                    $response['data']['message']['login']['danger'] = array_merge($response['data']['message']['login']['danger'], explode(PHP_EOL, trim($this->ion_auth->errors())));
+                    $response['data']['csrf']['name'] = $this->security->get_csrf_token_name();
+                    $response['data']['csrf']['hash'] = $this->security->get_csrf_hash();
+                }
+            }
+            else
+            {
+                $response['data']['status'] = 0;
+                $response['data']['message']['validation']['info'] = $this->validation_errors();
+                $response['data']['csrf']['name'] = $this->security->get_csrf_token_name();
+                $response['data']['csrf']['hash'] = $this->security->get_csrf_hash();
+            }
+        }
+
+        $response['status'] = \Restserver\Libraries\REST_Controller::HTTP_OK;
+        $this->response($response, $response['status']);
+    }
+
+}
+
+?>
