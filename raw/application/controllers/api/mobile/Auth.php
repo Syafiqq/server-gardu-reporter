73c02d6d368a44699a1d3bd1000a1dc1b6222f72
Syafiqq <syafiq.rezpector@gmail.com>
2017-07-29 18:28:40 +0700

    Move from mobile route

--- raw/application/controllers/api/mobile/Auth.php
+++ raw/application/controllers/api/mobile/Auth.php
@@ -70,59 +70,105 @@ class Auth extends \Restserver\Libraries\MY_REST_Controller
      */
     public function login_post($group = 'admin')
     {
-        $this->load->library('session');
+        $this->language = $this->getOrDefault('lang', $this->config->item('language'));
+
         /** @var array $response */
         $response = [];
+        $response['data']['status'] = 0;
+        log_message('ERROR', var_export(['post' => $this->post(),
+            'get' => $_GET], true));
 
-        if ($this->ion_auth->logged_in())
+        if (!empty($_SERVER['HTTP_X_ACCESS_PERMISSION']) && !empty($_SERVER['HTTP_X_ACCESS_GUARD']))
         {
-            $this->lang->load('ion_auth_extended', $this->language);
-
-            $response['data']['message']['login']['info'] = $this->lang->line('login_already_active');
-        }
-        else
-        {
-            /** @var array $data */
-            $data = [];
-
-            $data['identity'] = $this->postOrDefault('identity', null);
-            $data['password'] = $this->postOrDefault('password', null);
-
-            $this->lang->load(['common/auth/common_auth_login_form', 'auth'], $this->language);
-            $this->form_validation->set_data($data);
-
-            $this->form_validation->set_rules('identity', $this->lang->line('common_auth_login_form_email_label'), 'required|valid_email');
-            $this->form_validation->set_rules('password', $this->lang->line('common_auth_login_form_password_label'), 'required');
-
-            if ($this->form_validation->run() == true)
+            if (strcmp(strtolower($_SERVER['HTTP_X_ACCESS_PERMISSION']), $this->config->item('non_csrf_permission')) === 0)
             {
-                $remember = boolval(($this->postOrDefault('remember_me', false)));
+                if (strcmp(strtolower($_SERVER['HTTP_X_ACCESS_GUARD']), $this->config->item('non_csrf_guard')) === 0)
+                {
+                    $this->lang->load(['common/auth/common_auth_login_form', 'auth'], $this->language);
 
-                if ($this->ion_auth->login_and_check($data['identity'], $data['password'], $group, $remember))
-                {
-                    $response['data']['redirect'] = $this->postOrDefault('redirector', site_url('/dashboard'));
-                    $response['data']['status'] = 1;
-                    $flashdata = array_merge([], explode(PHP_EOL, trim($this->ion_auth->messages())));
-                    $this->session->set_flashdata(['flashdata' => ['message' => $flashdata]]);
+                    /** @var array $data */
+                    $data = [];
+
+                    $data['identity'] = $this->postOrDefault('identity', null);
+                    $data['password'] = $this->postOrDefault('password', null);
+
+                    $this->form_validation->set_data($data);
+
+                    $this->form_validation->set_rules('identity', $this->lang->line('common_auth_login_form_email_label'), 'required|valid_email');
+                    $this->form_validation->set_rules('password', $this->lang->line('common_auth_login_form_password_label'), 'required');
+
+                    if ($this->form_validation->run() == true)
+                    {
+                        $remember = false;
+
+                        if ($this->ion_auth->login_for_api($data['identity'], $data['password'], $group, $remember))
+                        {
+                            $response['data']['token'] = $this->ion_auth->getApiToken();
+                            $response['data']['status'] = 1;
+                            $response['data']['message'] = array_merge([], is_array($this->ion_auth->messages()) ? $this->ion_auth->messages() : explode(PHP_EOL, trim($this->ion_auth->messages())));
+                        }
+                        else
+                        {
+                            $response['data']['message'] = array_merge([], is_array($this->ion_auth->errors()) ? $this->ion_auth->errors() : explode(PHP_EOL, trim($this->ion_auth->errors())));
+                        }
+                    }
+                    else
+                    {
+                        $response['data']['message'] = $this->validation_errors();
+                    }
                 }
-                else
-                {
-                    $response['data']['status'] = 0;
-                    $response['data']['message']['login']['danger'] = [];
-                    $response['data']['message']['login']['danger'] = array_merge($response['data']['message']['login']['danger'], explode(PHP_EOL, trim($this->ion_auth->errors())));
-                    $response['data']['csrf']['name'] = $this->security->get_csrf_token_name();
-                    $response['data']['csrf']['hash'] = $this->security->get_csrf_hash();
-                }
-            }
-            else
-            {
-                $response['data']['status'] = 0;
-                $response['data']['message']['validation']['info'] = $this->validation_errors();
-                $response['data']['csrf']['name'] = $this->security->get_csrf_token_name();
-                $response['data']['csrf']['hash'] = $this->security->get_csrf_hash();
             }
         }
 
+        $response['status'] = \Restserver\Libraries\REST_Controller::HTTP_OK;
+        $this->response($response, $response['status']);
+    }
+
+    public function check_post()
+    {
+        $this->language = $this->getOrDefault('lang', $this->config->item('language'));
+
+        /** @var array $response */
+        $response = [];
+        $response['data']['status'] = 0;
+        log_message('ERROR', var_export(['post' => $this->post(),
+            'get' => $_GET], true));
+
+        if (!empty($_SERVER['HTTP_X_ACCESS_PERMISSION']) && !empty($_SERVER['HTTP_X_ACCESS_GUARD']))
+        {
+            if (strcmp(strtolower($_SERVER['HTTP_X_ACCESS_PERMISSION']), $this->config->item('non_csrf_permission')) === 0)
+            {
+                if (strcmp(strtolower($_SERVER['HTTP_X_ACCESS_GUARD']), $this->config->item('non_csrf_guard')) === 0)
+                {
+                    $this->lang->load(['common/auth/common_auth_token_form', 'auth'], $this->language);
+
+                    /** @var array $data */
+                    $data = [];
+
+                    $data['token'] = $this->postOrDefault('token', null);
+                    $this->form_validation->set_data($data);
+
+                    $this->form_validation->set_rules('token', $this->lang->line('common_auth_token_form_token_label'), 'required');
+
+                    if ($this->form_validation->run() == true)
+                    {
+                        if ($this->ion_auth->check_token($data['token']))
+                        {
+                            $response['data']['status'] = 1;
+                            $response['data']['message'] = array_merge([], is_array($this->ion_auth->messages()) ? $this->ion_auth->messages() : explode(PHP_EOL, trim($this->ion_auth->messages())));
+                        }
+                        else
+                        {
+                            $response['data']['message'] = array_merge([], is_array($this->ion_auth->errors()) ? $this->ion_auth->errors() : explode(PHP_EOL, trim($this->ion_auth->errors())));
+                        }
+                    }
+                    else
+                    {
+                        $response['data']['message'] = $this->validation_errors();
+                    }
+                }
+            }
+        }
         $response['status'] = \Restserver\Libraries\REST_Controller::HTTP_OK;
         $this->response($response, $response['status']);
     }
