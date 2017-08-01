e7878d3ec3ae44857ac30943bf805c6cfd8ed259
Syafiqq <syafiq.rezpector@gmail.com>
2017-08-01 22:45:02 +0700

    Filling common string

--- raw/application/language/english/layout/gardu/pengukuran/index/gardu_pengukuran_index_common_lang.php
+++ raw/application/language/english/layout/gardu/pengukuran/index/gardu_pengukuran_index_common_lang.php
@@ -5,4 +5,93 @@
  * Date / Time  : 01 August 2017, 8:35 AM.
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
- */ ?>
+ */
+
+$lang['gardu_pengukuran_index_common_creation_forbidden'] = 'You are not allowed to execute this operation';
+$lang['gardu_pengukuran_index_common_insert_success'] = 'Create Item Successfully';
+$lang['gardu_pengukuran_index_common_insert_failed'] = 'Create Item Failed';
+
+$lang['gardu_pengukuran_index_common_form_no_gardu_label'] = 'Gardu';
+$lang['gardu_pengukuran_index_common_form_nama_petugas1_label'] = 'Nama Petugas 1';
+$lang['gardu_pengukuran_index_common_form_nama_petugas2_label'] = 'Nama Petugas 2';
+$lang['gardu_pengukuran_index_common_form_no_kontrak_label'] = 'No Kontrak';
+$lang['gardu_pengukuran_index_common_form_arus_r_label'] = 'Arus R';
+$lang['gardu_pengukuran_index_common_form_arus_s_label'] = 'Arus S';
+$lang['gardu_pengukuran_index_common_form_arus_t_label'] = 'Arus T';
+$lang['gardu_pengukuran_index_common_form_arus_n_label'] = 'Arus N';
+$lang['gardu_pengukuran_index_common_form_teg_rn_label'] = 'Tegangan RN';
+$lang['gardu_pengukuran_index_common_form_teg_sn_label'] = 'Tegangan SN';
+$lang['gardu_pengukuran_index_common_form_teg_tn_label'] = 'Tegangan TN';
+$lang['gardu_pengukuran_index_common_form_teg_rs_label'] = 'Tegangan RS';
+$lang['gardu_pengukuran_index_common_form_teg_rt_label'] = 'Tegangan RT';
+$lang['gardu_pengukuran_index_common_form_teg_st_label'] = 'Tegangan ST';
+$lang['gardu_pengukuran_index_common_form_id_jurusan1_label'] = 'ID Jurusan Umum 1';
+$lang['gardu_pengukuran_index_common_form_arus_r_jurusan1_label'] = 'Arus R Umum 1';
+$lang['gardu_pengukuran_index_common_form_arus_s_jurusan1_label'] = 'Arus S Umum 1';
+$lang['gardu_pengukuran_index_common_form_arus_t_jurusan1_label'] = 'Arus T Umum 1';
+$lang['gardu_pengukuran_index_common_form_arus_n_jurusan1_label'] = 'Arus N Umum 1';
+$lang['gardu_pengukuran_index_common_form_teg_rn_jurusan1_label'] = 'Tegangan RN Umum 1';
+$lang['gardu_pengukuran_index_common_form_teg_sn_jurusan1_label'] = 'Tegangan SN Umum 1';
+$lang['gardu_pengukuran_index_common_form_teg_tn_jurusan1_label'] = 'Tegangan TN Umum 1';
+$lang['gardu_pengukuran_index_common_form_teg_rs_jurusan1_label'] = 'Tegangan RS Umum 1';
+$lang['gardu_pengukuran_index_common_form_teg_rt_jurusan1_label'] = 'Tegangan RT Umum 1';
+$lang['gardu_pengukuran_index_common_form_teg_st_jurusan1_label'] = 'Tegangan ST Umum 1';
+$lang['gardu_pengukuran_index_common_form_id_jurusan2_label'] = 'ID Jurusan Umum 2';
+$lang['gardu_pengukuran_index_common_form_arus_r_jurusan2_label'] = 'Arus R Umum 2';
+$lang['gardu_pengukuran_index_common_form_arus_s_jurusan2_label'] = 'Arus S Umum 2';
+$lang['gardu_pengukuran_index_common_form_arus_t_jurusan2_label'] = 'Arus T Umum 2';
+$lang['gardu_pengukuran_index_common_form_arus_n_jurusan2_label'] = 'Arus N Umum 2';
+$lang['gardu_pengukuran_index_common_form_teg_rn_jurusan2_label'] = 'Tegangan RN Umum 2';
+$lang['gardu_pengukuran_index_common_form_teg_sn_jurusan2_label'] = 'Tegangan SN Umum 2';
+$lang['gardu_pengukuran_index_common_form_teg_tn_jurusan2_label'] = 'Tegangan TN Umum 2';
+$lang['gardu_pengukuran_index_common_form_teg_rs_jurusan2_label'] = 'Tegangan RS Umum 2';
+$lang['gardu_pengukuran_index_common_form_teg_rt_jurusan2_label'] = 'Tegangan RT Umum 2';
+$lang['gardu_pengukuran_index_common_form_teg_st_jurusan2_label'] = 'Tegangan ST Umum 2';
+$lang['gardu_pengukuran_index_common_form_id_jurusan3_label'] = 'ID Jurusan Umum 3';
+$lang['gardu_pengukuran_index_common_form_arus_r_jurusan3_label'] = 'Arus R Umum 3';
+$lang['gardu_pengukuran_index_common_form_arus_s_jurusan3_label'] = 'Arus S Umum 3';
+$lang['gardu_pengukuran_index_common_form_arus_t_jurusan3_label'] = 'Arus T Umum 3';
+$lang['gardu_pengukuran_index_common_form_arus_n_jurusan3_label'] = 'Arus N Umum 3';
+$lang['gardu_pengukuran_index_common_form_teg_rn_jurusan3_label'] = 'Tegangan RN Umum 3';
+$lang['gardu_pengukuran_index_common_form_teg_sn_jurusan3_label'] = 'Tegangan SN Umum 3';
+$lang['gardu_pengukuran_index_common_form_teg_tn_jurusan3_label'] = 'Tegangan TN Umum 3';
+$lang['gardu_pengukuran_index_common_form_teg_rs_jurusan3_label'] = 'Tegangan RS Umum 3';
+$lang['gardu_pengukuran_index_common_form_teg_rt_jurusan3_label'] = 'Tegangan RT Umum 3';
+$lang['gardu_pengukuran_index_common_form_teg_st_jurusan3_label'] = 'Tegangan ST Umum 3';
+$lang['gardu_pengukuran_index_common_form_id_jurusan4_label'] = 'ID Jurusan Umum 4';
+$lang['gardu_pengukuran_index_common_form_arus_r_jurusan4_label'] = 'Arus R Umum 4';
+$lang['gardu_pengukuran_index_common_form_arus_s_jurusan4_label'] = 'Arus S Umum 4';
+$lang['gardu_pengukuran_index_common_form_arus_t_jurusan4_label'] = 'Arus T Umum 4';
+$lang['gardu_pengukuran_index_common_form_arus_n_jurusan4_label'] = 'Arus N Umum 4';
+$lang['gardu_pengukuran_index_common_form_teg_rn_jurusan4_label'] = 'Tegangan RN Umum 4';
+$lang['gardu_pengukuran_index_common_form_teg_sn_jurusan4_label'] = 'Tegangan SN Umum 4';
+$lang['gardu_pengukuran_index_common_form_teg_tn_jurusan4_label'] = 'Tegangan TN Umum 4';
+$lang['gardu_pengukuran_index_common_form_teg_rs_jurusan4_label'] = 'Tegangan RS Umum 4';
+$lang['gardu_pengukuran_index_common_form_teg_rt_jurusan4_label'] = 'Tegangan RT Umum 4';
+$lang['gardu_pengukuran_index_common_form_teg_st_jurusan4_label'] = 'Tegangan ST Umum 4';
+$lang['gardu_pengukuran_index_common_form_id_jurusank1_label'] = 'ID Jurusan Khusus 1';
+$lang['gardu_pengukuran_index_common_form_arus_r_jurusank1_label'] = 'Arus R Khusus 1';
+$lang['gardu_pengukuran_index_common_form_arus_s_jurusank1_label'] = 'Arus S Khusus 1';
+$lang['gardu_pengukuran_index_common_form_arus_t_jurusank1_label'] = 'Arus T Khusus 1';
+$lang['gardu_pengukuran_index_common_form_arus_n_jurusank1_label'] = 'Arus N Khusus 1';
+$lang['gardu_pengukuran_index_common_form_teg_rn_jurusank1_label'] = 'Tegangan RN Khusus 1';
+$lang['gardu_pengukuran_index_common_form_teg_sn_jurusank1_label'] = 'Tegangan SN Khusus 1';
+$lang['gardu_pengukuran_index_common_form_teg_tn_jurusank1_label'] = 'Tegangan TN Khusus 1';
+$lang['gardu_pengukuran_index_common_form_teg_rs_jurusank1_label'] = 'Tegangan RS Khusus 1';
+$lang['gardu_pengukuran_index_common_form_teg_rt_jurusank1_label'] = 'Tegangan RT Khusus 1';
+$lang['gardu_pengukuran_index_common_form_teg_st_jurusank1_label'] = 'Tegangan ST Khusus 1';
+$lang['gardu_pengukuran_index_common_form_id_jurusank2_label'] = 'ID Jurusan Khusus 2';
+$lang['gardu_pengukuran_index_common_form_arus_r_jurusank2_label'] = 'Arus R Khusus 2';
+$lang['gardu_pengukuran_index_common_form_arus_s_jurusank2_label'] = 'Arus S Khusus 2';
+$lang['gardu_pengukuran_index_common_form_arus_t_jurusank2_label'] = 'Arus T Khusus 2';
+$lang['gardu_pengukuran_index_common_form_arus_n_jurusank2_label'] = 'Arus N Khusus 2';
+$lang['gardu_pengukuran_index_common_form_teg_rn_jurusank2_label'] = 'Tegangan RN Khusus 2';
+$lang['gardu_pengukuran_index_common_form_teg_sn_jurusank2_label'] = 'Tegangan SN Khusus 2';
+$lang['gardu_pengukuran_index_common_form_teg_tn_jurusank2_label'] = 'Tegangan TN Khusus 2';
+$lang['gardu_pengukuran_index_common_form_teg_rs_jurusank2_label'] = 'Tegangan RS Khusus 2';
+$lang['gardu_pengukuran_index_common_form_teg_rt_jurusank2_label'] = 'Tegangan RT Khusus 2';
+$lang['gardu_pengukuran_index_common_form_teg_st_jurusank2_label'] = 'Tegangan ST Khusus 2';
+
+
+?>
+
