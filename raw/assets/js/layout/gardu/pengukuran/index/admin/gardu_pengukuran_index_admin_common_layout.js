fb7ea03290f8385a81d051564b1fbf60015d34c8
Syafiqq <syafiq.rezpector@gmail.com>
2017-08-01 22:45:20 +0700

    Completing inserting flow

--- raw/assets/js/layout/gardu/pengukuran/index/admin/gardu_pengukuran_index_admin_common_layout.js
+++ raw/assets/js/layout/gardu/pengukuran/index/admin/gardu_pengukuran_index_admin_common_layout.js
@@ -8,8 +8,8 @@
 
 (function ($) {
     $(function () {
+
         var retriever = $('meta[name="retriever"]').attr('content');
-        var location = {};
 
         var retreiveData = function (link, progress) {
             progress.start();
@@ -85,90 +85,105 @@
             var form = $(this);
 
             var input = form.serializeObject();
-            console.log(input);
-            return;
+            input = removeEmptyValues(input);
 
-            NProgress.start();
-            $.post(
-                form.attr('action'),
-                input,
-                null,
-                'json')
-                .done(function (response) {
-                    var kind = ['notify', 'message'];
-                    var type = ['validation', 'register'];
-                    var status = ['danger', 'info', 'warning', 'success'];
-                    if (response['data'] !== undefined)
-                    {
-                        if (response['data']['message'] !== undefined)
-                        {
-                            for (var i = -1, is = kind.length; ++i < is;)
-                            {
-                                if (response['data']['message'][kind[i]] !== undefined)
-                                {
-                                    for (var j = -1, js = type.length; ++j < js;)
+            $.confirm({
+                title: 'Konfirmasi!',
+                content: 'Dengan Ini data yang telah saya masukkan adalah benar dan dapat dipertanggungjawabkan',
+                theme: 'bootstrap',
+                buttons: {
+                    confirm: {
+                        btnClass: 'btn-blue',
+                        action: function () {
+                            NProgress.start();
+                            $.post(
+                                form.attr('action'),
+                                input,
+                                null,
+                                'json')
+                                .done(function (response) {
+                                    var kind = ['notify', 'message'];
+                                    var type = ['validation', 'register'];
+                                    var status = ['danger', 'info', 'warning', 'success'];
+                                    if (response['data'] !== undefined)
                                     {
-                                        if (response['data']['message'][kind[i]][type[j]] !== undefined)
+                                        if (response['data']['message'] !== undefined)
                                         {
-                                            for (var k = -1, ks = status.length; ++k < ks;)
+                                            for (var i = -1, is = kind.length; ++i < is;)
                                             {
-                                                if (response['data']['message'][kind[i]][type[j]][status[k]] !== undefined)
+                                                if (response['data']['message'][kind[i]] !== undefined)
                                                 {
-                                                    if (kind[i] === 'notify')
+                                                    for (var j = -1, js = type.length; ++j < js;)
                                                     {
-                                                        //noinspection JSDuplicatedDeclaration
-                                                        for (var l = -1, ls = response['data']['message'][kind[i]][type[j]][status[k]].length; ++l < ls;)
+                                                        if (response['data']['message'][kind[i]][type[j]] !== undefined)
                                                         {
-                                                            $.notify({
-                                                                message: response['data']['message'][kind[i]][type[j]][status[k]][l]
-                                                            }, {
-                                                                type: status[k]
-                                                            });
+                                                            for (var k = -1, ks = status.length; ++k < ks;)
+                                                            {
+                                                                if (response['data']['message'][kind[i]][type[j]][status[k]] !== undefined)
+                                                                {
+                                                                    if (kind[i] === 'notify')
+                                                                    {
+                                                                        //noinspection JSDuplicatedDeclaration
+                                                                        for (var l = -1, ls = response['data']['message'][kind[i]][type[j]][status[k]].length; ++l < ls;)
+                                                                        {
+                                                                            $.notify({
+                                                                                message: response['data']['message'][kind[i]][type[j]][status[k]][l]
+                                                                            }, {
+                                                                                type: status[k]
+                                                                            });
+                                                                        }
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        var template = '<div class="alert alert-' + status[k] + ' alert-dismissible">'
+                                                                            + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'
+                                                                            + '<ul>';
+                                                                        //noinspection JSDuplicatedDeclaration
+                                                                        for (var l = -1, ls = response['data']['message'][kind[i]][type[j]][status[k]].length; ++l < ls;)
+                                                                        {
+                                                                            template += '<li>' + response['data']['message'][kind[i]][type[j]][status[k]][l] + '</li>'
+                                                                        }
+                                                                        template += '</ul>'
+                                                                            + '</div>';
+                                                                        $("div#form-creation-message-container").empty().append(template);
+                                                                    }
+                                                                }
+                                                            }
                                                         }
-                                                    }
-                                                    else
-                                                    {
-                                                        var template = '<div class="alert alert-' + status[k] + ' alert-dismissible">'
-                                                            + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'
-                                                            + '<ul>';
-                                                        //noinspection JSDuplicatedDeclaration
-                                                        for (var l = -1, ls = response['data']['message'][kind[i]][type[j]][status[k]].length; ++l < ls;)
-                                                        {
-                                                            template += '<li>' + response['data']['message'][kind[i]][type[j]][status[k]][l] + '</li>'
-                                                        }
-                                                        template += '</ul>'
-                                                            + '</div>';
-                                                        $("div#form-creation-message-container").empty().append(template);
                                                     }
                                                 }
                                             }
                                         }
+
+                                        if (response['data']['csrf'] !== undefined)
+                                        {
+                                            $(form).find('input:hidden[name=' + response['data']['csrf']['name'] + ']').val(response['data']['csrf']['hash']);
+                                        }
+
+                                        if (response['data']['status'] !== undefined)
+                                        {
+                                            if (response['data']['status'] === 1)
+                                            {
+                                                $(form).find('input:text, input:password').each(function () {
+                                                    $(this).val('');
+                                                });
+                                            }
+                                        }
                                     }
-                                }
-                            }
-                        }
-
-                        if (response['data']['csrf'] !== undefined)
-                        {
-                            $(form).find('input:hidden[name=' + response['data']['csrf']['name'] + ']').val(response['data']['csrf']['hash']);
-                        }
-
-                        if (response['data']['status'] !== undefined)
-                        {
-                            if (response['data']['status'] === 1)
-                            {
-                                $(form).find('input:text, input:password').each(function () {
-                                    $(this).val('');
+                                })
+                                .fail(function () {
+                                })
+                                .always(function () {
+                                    NProgress.done();
                                 });
-                            }
                         }
+                    },
+                    cancel: function () {
                     }
-                })
-                .fail(function () {
-                })
-                .always(function () {
-                    NProgress.done();
-                });
+                }
+            });
+
+
         });
 
         if ((retriever !== undefined) && (retriever !== null))
