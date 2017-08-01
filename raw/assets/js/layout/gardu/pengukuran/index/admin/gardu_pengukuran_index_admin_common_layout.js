a93b4f9536f8abd013d8e4a9658d2a0bdbe1d86f
Syafiqq <syafiq.rezpector@gmail.com>
2017-08-01 15:41:45 +0700

    Add temporary insert measurement

--- raw/assets/js/layout/gardu/pengukuran/index/admin/gardu_pengukuran_index_admin_common_layout.js
+++ raw/assets/js/layout/gardu/pengukuran/index/admin/gardu_pengukuran_index_admin_common_layout.js
@@ -80,6 +80,97 @@
                 });
         };
 
+        $('form#create_index_measurement').on('submit', function (event) {
+            event.preventDefault();
+            var form = $(this);
+
+            var input = form.serializeObject();
+            console.log(input);
+            return;
+
+            NProgress.start();
+            $.post(
+                form.attr('action'),
+                input,
+                null,
+                'json')
+                .done(function (response) {
+                    var kind = ['notify', 'message'];
+                    var type = ['validation', 'register'];
+                    var status = ['danger', 'info', 'warning', 'success'];
+                    if (response['data'] !== undefined)
+                    {
+                        if (response['data']['message'] !== undefined)
+                        {
+                            for (var i = -1, is = kind.length; ++i < is;)
+                            {
+                                if (response['data']['message'][kind[i]] !== undefined)
+                                {
+                                    for (var j = -1, js = type.length; ++j < js;)
+                                    {
+                                        if (response['data']['message'][kind[i]][type[j]] !== undefined)
+                                        {
+                                            for (var k = -1, ks = status.length; ++k < ks;)
+                                            {
+                                                if (response['data']['message'][kind[i]][type[j]][status[k]] !== undefined)
+                                                {
+                                                    if (kind[i] === 'notify')
+                                                    {
+                                                        //noinspection JSDuplicatedDeclaration
+                                                        for (var l = -1, ls = response['data']['message'][kind[i]][type[j]][status[k]].length; ++l < ls;)
+                                                        {
+                                                            $.notify({
+                                                                message: response['data']['message'][kind[i]][type[j]][status[k]][l]
+                                                            }, {
+                                                                type: status[k]
+                                                            });
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        var template = '<div class="alert alert-' + status[k] + ' alert-dismissible">'
+                                                            + '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'
+                                                            + '<ul>';
+                                                        //noinspection JSDuplicatedDeclaration
+                                                        for (var l = -1, ls = response['data']['message'][kind[i]][type[j]][status[k]].length; ++l < ls;)
+                                                        {
+                                                            template += '<li>' + response['data']['message'][kind[i]][type[j]][status[k]][l] + '</li>'
+                                                        }
+                                                        template += '</ul>'
+                                                            + '</div>';
+                                                        $("div#form-creation-message-container").empty().append(template);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if (response['data']['csrf'] !== undefined)
+                        {
+                            $(form).find('input:hidden[name=' + response['data']['csrf']['name'] + ']').val(response['data']['csrf']['hash']);
+                        }
+
+                        if (response['data']['status'] !== undefined)
+                        {
+                            if (response['data']['status'] === 1)
+                            {
+                                $(form).find('input:text, input:password').each(function () {
+                                    $(this).val('');
+                                });
+                            }
+                        }
+                    }
+                })
+                .fail(function () {
+                })
+                .always(function () {
+                    NProgress.done();
+                });
+        });
+
         if ((retriever !== undefined) && (retriever !== null))
         {
             retreiveData(retriever, NProgress);
