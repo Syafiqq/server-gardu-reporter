4ab9dd572c049abf92cdab2ed100c22ae8df670c
Syafiqq <syafiq.rezpector@gmail.com>
2017-08-05 10:20:15 +0700

    Add rekap pengukuaran gardu

---
+++ raw/application/models/Model_rekap_pengukuran.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 05 August 2017, 6:24 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @property CI_DB_query_builder db
+ */
+class Model_rekap_pengukuran extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+    }
+
+    /**
+     * @param string $select
+     * @param null|string $from
+     * @param null|string $to
+     * @return CI_DB_result array
+     * @internal param array|null|string $id
+     */
+    public function find_gardu($select, $from = null, $to = null)
+    {
+        $this->db->select($select);
+        $this->db->from('`v_rekap_ukurgardufix`');
+        if (!is_null($from))
+        {
+            $this->db->where('`tgl_pengukuran` >=', $from);
+        }
+        if (!is_null($to))
+        {
+            $this->db->where('`tgl_pengukuran` <=', $to);
+        }
+        $this->db->order_by('`v_rekap_ukurgardufix`.`id_ukur_gardu`', 'ASC');
+        $response = $this->db->get();
+
+        return $response;
+    }
+
+
+}
+
+?>
