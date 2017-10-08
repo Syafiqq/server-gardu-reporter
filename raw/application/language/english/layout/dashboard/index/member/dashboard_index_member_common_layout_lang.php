4ac5c5d6d12df5b1e1abf6287396fa83ee867a27
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-08 21:46:45 +0700

    Initial phase of members dashboard

---
+++ raw/application/language/english/layout/dashboard/index/member/dashboard_index_member_common_layout_lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This <server-gardu-reporter> project created by :
+ * Name         : syafiq
+ * Date / Time  : 16 July 2017, 1:44 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+/** @var $lang array */
+$lang['dashboard_index_member_common_layout_welcome_message']                   = 'Welcome Back, %s!';
+$lang['dashboard_index_member_common_layout_page_title']                        = 'Sistem Pendataan Status Gardu Trafo';
+$lang['dashboard_index_member_common_layout_forbidden_access']                  = 'You Are Not Allowed Access This Page';
+$lang['dashboard_index_member_common_layout_forbidden_access_auth_redirection'] = 'Please Login First.';
+$lang['dashboard_index_member_common_layout_menu_add_entry']                    = 'Tambah Data Pengukuran';
+$lang['dashboard_index_member_common_layout_menu_add_gardu_centre']             = 'Tambah Data Gardu Induk';
+$lang['dashboard_index_member_common_layout_menu_add_feeder']                   = 'Tambah Data Penyulang';
+$lang['dashboard_index_member_common_layout_menu_add_gardu']                    = 'Tambah Data Gardu';
+?>
