2d6fcdb37a72f4cc169a4d24664e7d9c97d573ba
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-09 13:53:38 +0700

    Fix menu Fix admin -> member

--- raw/application/controllers/Rekap.php
+++ raw/application/controllers/Rekap.php
@@ -1164,13 +1164,13 @@ class Rekap extends CI_Controller
 
     private function _pengukuran_beban_imbang_member()
     {
-        $this->lang_prefix      = 'rekap_pengukuran_beban_imbang_admin';
+        $this->lang_prefix      = 'rekap_pengukuran_beban_imbang_member';
         $this->lang_layout      = 'common_layout';
-        $this->lang_prefix_path = 'rekap/pengukuran/beban/imbang/admin';
+        $this->lang_prefix_path = 'rekap/pengukuran/beban/imbang/member';
         $this->lang->load("layout/{$this->lang_prefix_path}/{$this->lang_prefix}_{$this->lang_layout}", $this->language);
         $this->lang->load("layout/rekap/pengukuran/beban/imbang/rekap_pengukuran_beban_imbang_common", $this->language);
 
-        if ($this->ion_auth->logged_in() && ($this->group === 'admin'))
+        if ($this->ion_auth->logged_in() && ($this->group === 'members'))
         {
             $this->load->helper('form');
 
@@ -1256,7 +1256,7 @@ class Rekap extends CI_Controller
 
             $_properties = compact('meta', 'string', 'data');
 
-            $view['sidebar']      = $this->load->view("common/common_menus_{$this->lang_layout}", $_properties, true);
+            $view['sidebar']      = $this->load->view("common/common_menus_member_{$this->lang_layout}", $_properties, true);
             $view['edit_profile'] = $this->load->view("common/profile/common_profile_edit_{$this->lang_layout}", $_properties, true);
 
             $_properties['view'] = $view;
@@ -1273,7 +1273,7 @@ class Rekap extends CI_Controller
                     , 'redirector' => site_url("/rekap/beban/imbang")
                 ]
             ]);
-            redirect('/auth/login/admin');
+            redirect('/auth/login/members');
         }
     }
 }
