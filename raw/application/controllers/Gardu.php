26b3a0e01bc0218bd420827e28765fcdcae1f247
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-09 10:30:34 +0700

    Initial phase of member gardu detail

--- raw/application/controllers/Gardu.php
+++ raw/application/controllers/Gardu.php
@@ -934,7 +934,140 @@ class Gardu extends CI_Controller
 
     private function _detail_member()
     {
+        $this->lang_prefix      = 'gardu_detail_admin';
+        $this->lang_layout      = 'common_layout';
+        $this->lang_prefix_path = 'gardu/detail/admin';
+        $this->lang->load("layout/{$this->lang_prefix_path}/{$this->lang_prefix}_{$this->lang_layout}", $this->language);
+        $this->lang->load("layout/gardu/index/gardu_index_common", $this->language);
 
+        $gardu = $this->input->get('gardu');
+        $this->load->model('model_gardu_index', 'mgidx');
+        if (empty($gardu))
+        {
+            $flashdata = [$this->lang->line("{$this->lang_prefix}_{$this->lang_layout}_no_gardu_filled")];
+            $this->session->set_flashdata(['flashdata' => ['message' => $flashdata]]);
+            redirect('/gardu');
+
+        }
+        else if (!$this->mgidx->id_check($gardu))
+        {
+            $flashdata = [$this->lang->line("{$this->lang_prefix}_{$this->lang_layout}_no_gardu_found")];
+            $this->session->set_flashdata(['flashdata' => ['message' => $flashdata]]);
+            redirect('/gardu');
+
+            return;
+        }
+        else if ($this->ion_auth->logged_in() && ($this->group === 'admin'))
+        {
+            $this->load->helper('form');
+
+            $this->lang->load('common/auth/common_auth_common', $this->language);
+            $this->lang->load('common/profile/common_profile_common', $this->language);
+            $this->lang->load('common/profile/edit/common_profile_edit_common', $this->language);
+            $this->lang->load('common/sidebar/common_sidebar_common', $this->language);
+            $this->lang->load('common/auth/common_auth_register_form', $this->language);
+            $this->lang->load('common', $this->language);
+
+            $string = [];
+            $meta   = [];
+            $data   = [];
+            $view   = [];
+
+            $_user                        = $this->ion_auth->user()->row_array();
+            $data['profile']['username']  = $_user['username'];
+            $data['profile']['email']     = $_user['email'];
+            $data['profile']['group']     = 'Admin';
+            $data['update']['redirector'] = site_url("/gardu/detail?gardu={$gardu}");
+
+            //Core Data
+            $string['title']        = $this->lang->line('common_title');
+            $string['page_title']   = $this->lang->line("{$this->lang_prefix}_{$this->lang_layout}_page_title");
+            $string['auth_logout']  = $this->lang->line('common_auth_common_logout');
+            $string['profile_edit'] = $this->lang->line('common_profile_common_edit_button');
+
+            //Sidebar
+            $string['sidebar_home']                        = $this->lang->line('common_sidebar_common_sidebar_home');
+            $string['sidebar_recapitulation']              = $this->lang->line('common_sidebar_common_sidebar_recapitulation');
+            $string['sidebar_recapitulation_measurement']  = $this->lang->line('common_sidebar_common_sidebar_recapitulation_measurement');
+            $string['sidebar_recapitulation_voltage_end']  = $this->lang->line('common_sidebar_common_sidebar_recapitulation_voltage_end');
+            $string['sidebar_recapitulation_travo_load']   = $this->lang->line('common_sidebar_common_sidebar_recapitulation_travo_load');
+            $string['sidebar_recapitulation_load_balance'] = $this->lang->line('common_sidebar_common_sidebar_recapitulation_load_balance');
+            $string['sidebar_info_gardu']                  = $this->lang->line('common_sidebar_common_sidebar_info_gardu');
+            $string['sidebar_info_gardu_hq']               = $this->lang->line('common_sidebar_common_sidebar_info_gardu_hq');
+            $string['sidebar_info_gardu_feeder']           = $this->lang->line('common_sidebar_common_sidebar_info_gardu_feeder');
+            $string['sidebar_info_gardu_data']             = $this->lang->line('common_sidebar_common_sidebar_info_gardu_data');
+            $string['sidebar_info_user_management']        = $this->lang->line('common_sidebar_common_sidebar_info_user_management');
+
+            //Profile
+            $string['client_register']                  = $this->lang->line('common_auth_common_register');
+            $string['client_form_username_label']       = $this->lang->line('common_auth_register_form_username_label');
+            $string['client_form_email_label']          = $this->lang->line('common_auth_register_form_email_label');
+            $string['client_form_role_label']           = $this->lang->line('common_auth_register_form_role_label');
+            $string['client_form_username_placeholder'] = $this->lang->line("common_profile_edit_common_client_username_placeholder");
+            $string['client_form_email_placeholder']    = $this->lang->line("common_profile_edit_common_client_email_placeholder");
+            $string['inline_client_form_id_id']         = 'id';
+            $string['inline_client_form_username_id']   = 'username';
+            $string['inline_client_form_email_id']      = 'email';
+            $string['inline_client_form_role_id']       = 'role';
+
+            $string['item']['label']['induk_id']     = $this->lang->line("gardu_index_common_form_induk_id_label");
+            $string['item']['label']['penyulang_id'] = $this->lang->line("gardu_index_common_form_penyulang_id_label");
+            $string['item']['label']['jenis']        = $this->lang->line("gardu_index_common_form_jenis_label");
+            $string['item']['label']['no']           = $this->lang->line("gardu_index_common_form_no_label");
+            $string['item']['label']['lokasi']       = $this->lang->line("gardu_index_common_form_lokasi_label");
+            $string['item']['label']['merk']         = $this->lang->line("gardu_index_common_form_merk_label");
+            $string['item']['label']['serial']       = $this->lang->line("gardu_index_common_form_serial_label");
+            $string['item']['label']['daya']         = $this->lang->line("gardu_index_common_form_daya_label");
+            $string['item']['label']['fasa']         = $this->lang->line("gardu_index_common_form_fasa_label");
+            $string['item']['label']['tap']          = $this->lang->line("gardu_index_common_form_tap_label");
+            $string['item']['label']['jurusan']      = $this->lang->line("gardu_index_common_form_jurusan_label");
+            $string['item']['label']['lat']          = $this->lang->line("gardu_index_common_form_lat_label");
+            $string['item']['label']['long']         = $this->lang->line("gardu_index_common_form_long_label");
+
+            $string['item']['id']['induk_id']     = 'induk_id';
+            $string['item']['id']['penyulang_id'] = 'penyulang_id';
+            $string['item']['id']['jenis']        = 'jenis';
+            $string['item']['id']['no']           = 'no';
+            $string['item']['id']['lokasi']       = 'lokasi';
+            $string['item']['id']['merk']         = 'merk';
+            $string['item']['id']['serial']       = 'serial';
+            $string['item']['id']['daya']         = 'daya';
+            $string['item']['id']['fasa']         = 'fasa';
+            $string['item']['id']['tap']          = 'tap';
+            $string['item']['id']['jurusan']      = 'jurusan';
+            $string['item']['id']['lat']          = 'lat';
+            $string['item']['id']['long']         = 'long';
+
+            $string['map']['title']                = $this->lang->line("{$this->lang_prefix}_{$this->lang_layout}_map_title");
+            $string['map']['button']['show_route'] = $this->lang->line("{$this->lang_prefix}_{$this->lang_layout}_map_button_show_route");
+
+            $meta['retriever'] = site_url("/api/gardu/index/detail?gardu={$gardu}");
+
+            $data['meta']['i18n']['country']  = empty($data['meta']['i18n']['country'] = i18nGetCountryCode($this->country)) ? 'US' : $data['meta']['i18n']['country'];
+            $data['meta']['i18n']['language'] = empty($data['meta']['i18n']['language'] = i18nGetLanguageCode($this->language)) ? 'en' : $data['meta']['i18n']['language'];
+            $data['session']['flashdata']     = empty(@$this->session->userdata('flashdata')['message']) ? [] : $this->session->userdata('flashdata')['message'];
+
+            $_properties = compact('meta', 'string', 'data');
+
+            $view['sidebar']      = $this->load->view("common/common_menus_{$this->lang_layout}", $_properties, true);
+            $view['edit_profile'] = $this->load->view("common/profile/common_profile_edit_{$this->lang_layout}", $_properties, true);
+
+            $_properties['view'] = $view;
+            $this->load->view("{$this->lang_prefix_path}/{$this->lang_prefix}_{$this->lang_layout}", $_properties);
+        }
+        else
+        {
+            $this->session->set_flashdata([
+                'flashdata' => [
+                    'message' => [
+                        $this->lang->line("{$this->lang_prefix}_{$this->lang_layout}_forbidden_access"),
+                        $this->lang->line("{$this->lang_prefix}_{$this->lang_layout}_forbidden_access_auth_redirection")
+                    ]
+                    , 'redirector' => site_url("/{$this->lang_prefix_path}?gardu={$gardu}")
+                ]
+            ]);
+            redirect('/auth/login/admin');
+        }
     }
 
     public function pengukuran($gardu = 'index')
